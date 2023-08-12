969b4694d30d8789220e19ec03c0b8432f9f88f7
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2023-08-12 14:40:43 +0600

    changes for deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import { NextPage } from 'next';
 
-import Header from '@/components/sections/Header';
-import Hero from '@/components/sections/Hero';
-import About from '@/components/sections/About';
+import Header from './../components/sections/Header';
+import Hero from './../components/sections/Hero';
+import About from './../components/sections/About';
+
 import Testimonial from '@/components/sections/Testimonial';
 import Project from '@/components/sections/ProjectTab';
 import Contact from '@/components/sections/Contact';
