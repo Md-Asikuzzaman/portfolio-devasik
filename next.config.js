0c27f3d0c93024b2dfa7c524637205fcaab69c58
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2023-12-23 10:36:41 +0600

    added new setup 14

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
-/** @type {import('next').NextConfig} */
-
-const nextConfig = {
-  images: {
-    domains: ['raw.githubusercontent.com'],
-  },
+module.exports = async (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'raw.githubusercontent.com',
+        },
+      ],
+    },
+  };
+  return nextConfig;
 };
-
-module.exports = nextConfig;
