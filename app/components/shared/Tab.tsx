d91339a4833b5011bcda1773f8153dd69d48c747
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2024-01-12 23:08:19 +0600

    update tabs logic

--- app/components/shared/Tab.tsx
+++ app/components/shared/Tab.tsx
@@ -6,71 +6,47 @@ import AboutMe from './tab-options/AboutMe';
 import Skill from './tab-options/Skill';
 import Experience from './tab-options/Experience';
 import Education from './tab-options/Education';
+import TabButton from './TabButton';
 
 const Tab = () => {
-  const [tab, setTab] = useState(1);
+  const [currentTab, setCurrentTab] = useState(1);
 
-  const handleTab = (id: any) => {
-    setTab(id);
-  };
   return (
     <div className='col-start-2 col-end-4'>
       <div className='text-sm font-medium text-center text-zinc-400'>
-        <ul className='flex flex-wrap mb-8 lg:mb-12'>
-          <li className='mr-2'>
-            <button
-              onClick={() => handleTab(1)}
-              className={`inline-block p-2 lg:p-4 border-b-2 rounded-t-lg text-md md:text-lg ${
-                tab == 1
-                  ? 'text-pink-500 border-b-2 border-pink-500 rounded-t-lg active'
-                  : 'hover:text-gray-300 hover:border-gray-300 border-transparent'
-              } `}
-            >
-              About Me
-            </button>
-          </li>
-          <li className='mr-2'>
-            <button
-              onClick={() => handleTab(2)}
-              className={`inline-block p-2 lg:p-4 border-b-2 rounded-t-lg text-md md:text-lg  ${
-                tab == 2
-                  ? 'text-pink-500 border-b-2 border-pink-500 rounded-t-lg active'
-                  : 'hover:text-gray-300 hover:border-gray-300 border-transparent'
-              } `}
-            >
-              Skills
-            </button>
-          </li>
-          <li className='mr-2'>
-            <button
-              onClick={() => handleTab(3)}
-              className={`inline-block p-2 lg:p-4 border-b-2 rounded-t-lg text-md md:text-lg ${
-                tab == 3
-                  ? 'text-pink-500 border-b-2 border-pink-500 rounded-t-lg active'
-                  : 'hover:text-gray-300 hover:border-gray-300 border-transparent'
-              } `}
-            >
-              Experience
-            </button>
-          </li>
-          <li className='mr-2'>
-            <button
-              onClick={() => handleTab(4)}
-              className={`inline-block p-2 lg:p-4 border-b-2 rounded-t-lg text-md md:text-lg  ${
-                tab == 4
-                  ? 'text-pink-500 border-b-2 border-pink-500 rounded-t-lg active'
-                  : 'hover:text-gray-300 hover:border-gray-300 border-transparent'
-              } `}
-            >
-              Education
-            </button>
-          </li>
-        </ul>
+        <div className='flex flex-wrap gap-2 mb-8 lg:mb-12'>
+          <TabButton
+            title='About Me'
+            tabValue={1}
+            currentTab={currentTab}
+            setCurrentTab={setCurrentTab}
+          />
+
+          <TabButton
+            title='Skill'
+            tabValue={2}
+            currentTab={currentTab}
+            setCurrentTab={setCurrentTab}
+          />
+          <TabButton
+            title='Experience'
+            tabValue={3}
+            currentTab={currentTab}
+            setCurrentTab={setCurrentTab}
+          />
+
+          <TabButton
+            title='Education'
+            tabValue={4}
+            currentTab={currentTab}
+            setCurrentTab={setCurrentTab}
+          />
+        </div>
       </div>
-      {tab == 1 && <AboutMe />}
-      {tab == 2 && <Skill />}
-      {tab == 3 && <Experience />}
-      {tab == 4 && <Education />}
+      {currentTab == 1 && <AboutMe />}
+      {currentTab == 2 && <Skill />}
+      {currentTab == 3 && <Experience />}
+      {currentTab == 4 && <Education />}
     </div>
   );
 };
---
+++ app/components/shared/TabButton.tsx
@@ -0,0 +1,32 @@
+import { NextPage } from 'next';
+
+interface Props {
+  title: string;
+  tabValue: number;
+  currentTab: number;
+  setCurrentTab: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const TabButton: NextPage<Props> = ({
+  title,
+  tabValue,
+  currentTab,
+  setCurrentTab,
+}) => {
+  return (
+    <button
+      onClick={() => {
+        setCurrentTab(tabValue);
+      }}
+      className={`inline-block p-2 lg:p-4 border-b-2 rounded-t-lg text-md md:text-lg ${
+        currentTab == tabValue
+          ? 'text-pink-500 border-b-2 border-pink-500 rounded-t-lg active'
+          : 'hover:text-gray-300 hover:border-gray-300 border-transparent'
+      } `}
+    >
+      {title}
+    </button>
+  );
+};
+
+export default TabButton;
