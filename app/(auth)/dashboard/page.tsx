4e60bf7175a2419c300d1616b4e640185f5bcbf4
md-asikuzzaman <mdasikuzzaman.en@gmail.com>
2024-02-25 14:57:54 +0600

    fix responsive issues

--- app/(auth)/dashboard/page.tsx
+++ app/(auth)/dashboard/page.tsx
@@ -1,14 +1,14 @@
-'use client';
+"use client";
 
-import useProject from '@/hooks/useProjects';
-import axios from 'axios';
-import { NextPage } from 'next';
-import Image from 'next/image';
-import Link from 'next/link';
-import { useCallback, useEffect } from 'react';
-import { toast } from 'react-toastify';
+import useProject from "@/hooks/useProjects";
+import axios from "axios";
+import { NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import { useCallback, useEffect } from "react";
+import { toast } from "react-toastify";
 
-import { AiFillDelete, AiFillEdit } from 'react-icons/ai';
+import { AiFillDelete, AiFillEdit } from "react-icons/ai";
 
 interface Props {}
 
@@ -21,11 +21,11 @@ const Page: NextPage<Props> = ({}) => {
 
   const handleClick = useCallback(
     async (id: string) => {
-      if (window.confirm('Do you really want to delete?')) {
+      if (window.confirm("Do you really want to delete?")) {
         const res = await axios.delete(`/api/projects/delete/${id}`);
 
         if (res.status === 200) {
-          toast.error('Project deleted!!!');
+          toast.error("Project deleted!!!");
         }
 
         mutate();
@@ -36,85 +36,72 @@ const Page: NextPage<Props> = ({}) => {
 
   return (
     <div>
-      <h2 className='text-xl md:text-2xl font-semibold mb-4'>All Projects</h2>
-      <div className='flex flex-col gap-3'>
-        <div className='relative overflow-x-scroll shadow-md sm:rounded-lg'>
-          <table className='w-full text-sm text-left text-gray-500'>
-            <thead className='text-xs text-gray-700 uppercase bg-gray-50 '>
+      <h2 className="text-xl md:text-2xl font-semibold mb-4">All Projects</h2>
+      <div className="flex flex-col gap-3">
+        {/* table start */}
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
               <tr>
-                <th scope='col' className='px-6 py-3'>
+                <th scope="col" className="px-6 py-3">
                   Title
                 </th>
-                <th scope='col' className='px-6 py-3'>
+                <th scope="col" className="px-6 py-3">
                   Description
                 </th>
-                <th scope='col' className='px-6 py-3'>
-                  variant
+                <th scope="col" className="px-6 py-3">
+                  Variant
                 </th>
-                <th scope='col' className='px-6 py-3'>
-                  Image
+                <th scope="col" className="px-6 py-3">
+                  image
                 </th>
-                <th scope='col' className='px-6 py-3'>
+                <th scope="col" className="px-6 py-3">
                   Action
                 </th>
               </tr>
             </thead>
             <tbody>
-              {isLoading ? (
-                <tr className='bg-white border-b  hover:bg-gray-50'>
-                  <td className='px-6 py-4 text-center' colSpan={5}>
-                    Loading...
+              {data?.map(({ id, title, description, variant, image }) => (
+                <tr key={id} className="bg-white border-b  hover:bg-gray-50">
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {title}
+                  </th>
+                  <td className="px-6 py-4">{description.slice(0, 20)}</td>
+                  <td className="px-6 py-4">{variant}</td>
+                  <td className="p-2">
+                    <Image
+                      height={20}
+                      width={80}
+                      className="bg-cover bg-center"
+                      src={image}
+                      alt="cover"
+                    />
+                  </td>
+                  <td className="px-6 py-4 flex items-center gap-4">
+                    <Link
+                      href={`/dashboard/update/${id}`}
+                      className="flex items-center gap-1 font-medium text-blue-600 hover:underline"
+                    >
+                      Edit
+                      <AiFillEdit size={18} />
+                    </Link>
+                    <button
+                      onClick={() => handleClick(id)}
+                      className="flex items-center gap-1 font-medium text-rose-600 hover:underline"
+                    >
+                      Delete
+                      <AiFillDelete size={18} />
+                    </button>
                   </td>
                 </tr>
-              ) : (
-                data.map((project) => (
-                  <tr
-                    key={project.id}
-                    className='bg-white border-b  hover:bg-gray-50'
-                  >
-                    <th
-                      scope='row'
-                      className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap'
-                    >
-                      {project.title}
-                    </th>
-                    <td className='px-6 py-4'>
-                      {project.description.slice(0, 20)}
-                    </td>
-                    <td className='px-6 py-4'>{project.variant}</td>
-
-                    <td className='p-2'>
-                      <Image
-                        height={20}
-                        width={80}
-                        className='bg-cover bg-center'
-                        src={project.image}
-                        alt='cover'
-                      />
-                    </td>
-                    <td className='px-6 py-4 flex items-center gap-4'>
-                      <Link
-                        href={`/dashboard/update/${project.id}`}
-                        className='flex items-center gap-1 font-medium text-blue-600 hover:underline'
-                      >
-                        Edit
-                        <AiFillEdit size={18} />
-                      </Link>
-
-                      <button
-                        onClick={() => handleClick(project.id)}
-                        className='flex items-center gap-1 font-medium text-rose-600 hover:underline'
-                      >
-                        Delete
-                        <AiFillDelete size={18} />
-                      </button>
-                    </td>
-                  </tr>
-                ))
-              )}
+              ))}
             </tbody>
           </table>
         </div>
+        {/* table end */}
       </div>
     </div>
   );
