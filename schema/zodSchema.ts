a616c64251a68d1367e6475d2d2c025cc075656a
md-asikuzzaman <mdasikuzzaman.en@example.com>
2024-03-25 13:19:16 +0600

    add trim method inside mailschema

--- schema/zodSchema.ts
+++ schema/zodSchema.ts
@@ -3,11 +3,13 @@ import { z } from "zod";
 export const mailSchema = z.object({
   email: z
     .string()
+    .trim()
     .min(1, { message: "Email is required" })
     .email("Invalid email address"),
 
   message: z
     .string()
+    .trim()
     .min(1, { message: "Message is required" })
     .max(300, { message: "Message at most 300 char." }),
 });
