b0fbe1326cb124dc37f17a436e8db5c1dee8d811
Asik <mdasikuzzaman.en@gmail.com>
2024-04-11 02:53:53 +0600

    Update TabButton

--- app/components/shared/TabButton.tsx
+++ app/components/shared/TabButton.tsx
@@ -18,9 +18,9 @@ const TabButton: NextPage<Props> = ({
       onClick={() => {
         setCurrentTab(tabValue);
       }}
-      className={`inline-block p-2 lg:p-3 border-b-2 rounded-t-lg text-md md:text-lg ${
+      className={`inline-block p-2 lg:p-3 border-b-2 rounded-lg text-md md:text-lg ${
         currentTab == tabValue
-          ? 'text-pink-500 bg-pink-500/10 border-b-2 border-pink-500 rounded-t-lg active'
+          ? 'text-pink-500 bg-pink-500/10 border border-pink-500 rounded-lg active'
           : 'hover:text-gray-300 hover:border-gray-300 border-transparent'
       } `}
     >
