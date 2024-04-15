00b3a2491b322654d646563a3794d31bb0d38072
md-asikuzzaman <mdasikuzzaman.en@example.com>
2024-04-15 12:34:23 +0600

    solve tab color combinations

--- app/components/shared/TabButton.tsx
+++ app/components/shared/TabButton.tsx
@@ -1,4 +1,4 @@
-import { NextPage } from 'next';
+import { NextPage } from "next";
 
 interface Props {
   title: string;
@@ -20,8 +20,8 @@ const TabButton: NextPage<Props> = ({
       }}
       className={`inline-block p-2 lg:p-3 border-b-2 rounded-t-lg text-md md:text-lg ${
         currentTab == tabValue
-          ? 'text-pink-500 bg-pink-500/10 border border-pink-500 rounded-lg active'
-          : 'hover:text-gray-300 hover:border-gray-300 border-transparent'
+          ? "text-pink-500 bg-pink-500/10 border-b-2 border-pink-500 rounded-t-lg active"
+          : "hover:text-gray-300 hover:border-gray-300 border-transparent"
       } `}
     >
       {title}
