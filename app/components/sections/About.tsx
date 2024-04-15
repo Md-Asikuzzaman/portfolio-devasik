be80aeb083de07a66f7c392b5dcfef6e84f16de4
md-asikuzzaman <mdasikuzzaman.en@example.com>
2024-04-15 15:02:37 +0600

    modify dev image size and radius and more

--- app/components/sections/About.tsx
+++ app/components/sections/About.tsx
@@ -1,13 +1,13 @@
-'use client';
+"use client";
 
-import { useEffect } from 'react';
-import { useInView } from 'react-intersection-observer';
-import { useActiveSection } from '@/hooks/useActiveSection';
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import { useActiveSection } from "@/hooks/useActiveSection";
 
-import Image from 'next/image';
+import Image from "next/image";
 
-import Tab from '@/app/components/shared/Tab';
-import TestimonialSlider from '@/app/components/sections/TestimonialSlider';
+import Tab from "@/app/components/shared/Tab";
+import TestimonialSlider from "@/app/components/sections/TestimonialSlider";
 
 const About = () => {
   const { ref, inView } = useInView({
@@ -17,30 +17,35 @@ const About = () => {
 
   useEffect(() => {
     if (inView) {
-      setActiveSection('about');
+      setActiveSection("about");
     }
   }, [inView, setActiveSection]);
 
   return (
-    <section ref={ref} id='about' className='relative overflow-hidden lg:pb-10'>
-      <div className='relative -z-50 pt-12'>
-        <div className='flex flex-row justify-center'>
-          <div className='absolute top-0 h-[1px] w-full bg-gradient-to-r from-transparent via-[#262B42] to-transparent'></div>
-          <div className='w-[300px] h-[300px] bg-violet-500/80 rounded-full absolute -top-[150px] filter blur-3xl  opacity-20'></div>
+    <section ref={ref} id="about" className="relative overflow-hidden lg:pb-10">
+      <div className="relative -z-50 pt-12">
+        <div className="flex flex-row justify-center">
+          <div className="absolute top-0 h-[1px] w-full bg-gradient-to-r from-transparent via-[#262B42] to-transparent"></div>
+          <div className="w-[300px] h-[300px] bg-violet-500/80 rounded-full absolute -top-[150px] filter blur-3xl  opacity-20"></div>
         </div>
       </div>
-      <div className='container'>
-        <h2 className='text-2xl md:text-3xl text-center font-bold text-white mb-10'>
+      <div className="container">
+        <h2 className="text-2xl md:text-3xl text-center font-bold text-white mb-10">
           About Me
         </h2>
-        <div className='flex flex-col md:flex-row items-center justify-center gap-10'>
-          <Image
-            className='rounded-full'
-            src={'/images/dev.jpg'}
-            alt='dev'
-            height='250'
-            width='250'
-          />
+        <div className="flex flex-col md:flex-row items-center justify-center gap-10">
+          <div className="relative w-[280px] h-[280px] md:w-[250px] md:h-[250px] lg:w-[350px] lg:h-[350px] grayscale hover:grayscale-0">
+            <Image
+              title="Don't touch me! 😂"
+              className="rounded-[40px] cursor-grab"
+              src={"/images/dev.jpg"}
+              alt="dev"
+              fill
+              style={{
+                backgroundSize: "cover",
+              }}
+            />
+          </div>
           <Tab />
         </div>
       </div>
