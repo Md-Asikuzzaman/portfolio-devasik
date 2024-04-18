982e7a0f77f3859dbeca6125a919b6e116df6230
md-asikuzzaman <mdasikuzzaman.en@example.com>
2024-04-18 16:34:40 +0600

    added delete project server type

--- app/(root)/dashboard/update/[id]/page.tsx
+++ app/(root)/dashboard/update/[id]/page.tsx
@@ -1,15 +1,19 @@
-'use client';
+"use client";
 
-import { useState, useEffect } from 'react';
-import MyInput from '@/app/components/shared/Input';
-import axios from 'axios';
-import { useRouter } from 'next/navigation';
-import { NextPage } from 'next';
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import toast from 'react-hot-toast';
+import { useState, useEffect } from "react";
+import MyInput from "@/app/components/shared/Input";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { NextPage } from "next";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 interface Props {
   params: { id: string };
+}
+
+interface QueryResponse {
+  project: ProjectType;
 }
 
 const Page: NextPage<Props> = ({ params }) => {
@@ -19,24 +23,24 @@ const Page: NextPage<Props> = ({ params }) => {
   const router = useRouter();
   const queryClient = useQueryClient();
 
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [image, setImage] = useState('');
-  const [website, setWebsite] = useState('');
-  const [github, setGithub] = useState('');
-  const [variant, setVariant] = useState('');
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState("");
+  const [website, setWebsite] = useState("");
+  const [github, setGithub] = useState("");
+  const [variant, setVariant] = useState("");
 
   const resetForm = () => {
-    setTitle('');
-    setDescription('');
-    setImage('');
-    setWebsite('');
-    setGithub('');
-    setVariant('');
+    setTitle("");
+    setDescription("");
+    setImage("");
+    setWebsite("");
+    setGithub("");
+    setVariant("");
   };
 
-  const { data: project, isLoading } = useQuery<ProjectType>({
-    queryKey: ['fetch_project_byId', id],
+  const { data, isLoading } = useQuery<QueryResponse>({
+    queryKey: ["fetch_project_byId", id],
     queryFn: async () => {
       const res = await axios.get(`/api/projects/${id}`);
       return res.data;
@@ -44,28 +48,28 @@ const Page: NextPage<Props> = ({ params }) => {
   });
 
   useEffect(() => {
-    if (project) {
-      setTitle(project.title);
-      setDescription(project.description);
-      setImage(project.image);
-      setWebsite(project.website);
-      setGithub(project.github);
-      setVariant(project.variant);
+    if (data?.project) {
+      setTitle(data?.project.title);
+      setDescription(data?.project.description);
+      setImage(data?.project.image);
+      setWebsite(data?.project.website);
+      setGithub(data?.project.github);
+      setVariant(data?.project.variant);
     }
-  }, [id, project]);
+  }, [id, data]);
 
   const { mutate, isPending } = useMutation({
-    mutationKey: ['update_project'],
+    mutationKey: ["update_project"],
     mutationFn: async (data: ProjectType) => {
       await axios.put(`/api/projects/update/${id}`, data);
     },
 
     onSuccess: () => {
       queryClient.invalidateQueries({
-        queryKey: ['fetch_projects'],
+        queryKey: ["fetch_projects"],
       });
-      toast.success('Project updated!!!');
-      router.replace('/dashboard');
+      toast.success("Project updated!!!");
+      router.replace("/dashboard");
       resetForm();
     },
   });
@@ -77,61 +81,61 @@ const Page: NextPage<Props> = ({ params }) => {
   };
 
   return (
-    <div className='flex items-start justify-center min-h-screen w-full md:mt-12'>
+    <div className="flex items-start justify-center min-h-screen w-full md:mt-12">
       <form
         onSubmit={handleSubmit}
-        className='bg-white max-w-2xl w-full p-12 rounded-2xl shadow-lg'
+        className="bg-white max-w-2xl w-full p-12 rounded-2xl shadow-lg"
       >
-        <h2 className='text-xl md:text-2xl font-semibold mb-4'>
-          {isLoading ? 'Loading...' : 'Update Project'}
+        <h2 className="text-xl md:text-2xl font-semibold mb-4">
+          {isLoading ? "Loading..." : "Update Project"}
         </h2>
         <MyInput
-          variant='bw'
-          label='Title'
-          id='title'
+          variant="bw"
+          label="Title"
+          id="title"
           value={title}
           data={setTitle}
         />
         <MyInput
-          variant='bw'
-          label='Description'
-          id='description'
+          variant="bw"
+          label="Description"
+          id="description"
           value={description}
           data={setDescription}
         />
         <MyInput
-          variant='bw'
-          label='Image link'
-          id='image'
+          variant="bw"
+          label="Image link"
+          id="image"
           value={image}
           data={setImage}
         />
         <MyInput
-          variant='bw'
-          label='Site link'
-          id='site'
+          variant="bw"
+          label="Site link"
+          id="site"
           value={website}
           data={setWebsite}
         />
         <MyInput
-          variant='bw'
-          label='Github link'
-          id='github'
+          variant="bw"
+          label="Github link"
+          id="github"
           value={github}
           data={setGithub}
         />
         <MyInput
-          variant='bw'
-          label='Variant'
-          id='variant'
+          variant="bw"
+          label="Variant"
+          id="variant"
           value={variant}
           data={setVariant}
         />
         <button
-          className='w-full py-3 rounded-md bg-neutral-900 text-white hover:bg-neutral-800 mt-4 transition'
-          type='submit'
+          className="w-full py-3 rounded-md bg-neutral-900 text-white hover:bg-neutral-800 mt-4 transition"
+          type="submit"
         >
-          {isPending ? 'Modifying...' : "Let's Modify"}
+          {isPending ? "Modifying..." : "Let's Modify"}
         </button>
       </form>
     </div>
--- app/api/projects/[id]/route.ts
+++ app/api/projects/[id]/route.ts
@@ -1,28 +1,33 @@
-import prisma from '@/lib/db';
-import { NextResponse } from 'next/server';
+import prisma from "@/lib/db";
+import { NextResponse } from "next/server";
+
+interface ApiResponse {
+  project?: ProjectType | null;
+  deletedProject?: ProjectType;
+}
 
 export async function GET(
   request: Request,
   context: { params: { id: string } }
-) {
+): Promise<NextResponse<ApiResponse>> {
   const { id } = context.params;
 
   const project = await prisma.project.findUnique({
     where: { id: id },
   });
 
-  return NextResponse.json(project, { status: 200 });
+  return NextResponse.json({ project }, { status: 200 });
 }
 
 export async function DELETE(
   request: Request,
   context: { params: { id: string } }
-) {
+): Promise<NextResponse<ApiResponse>> {
   const { id } = context.params;
 
-  const res = await prisma.project.delete({
+  const deletedProject = await prisma.project.delete({
     where: { id: id },
   });
 
-  return NextResponse.json(res, { status: 200 });
+  return NextResponse.json({ deletedProject }, { status: 200 });
 }
