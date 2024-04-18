328ff7ea5fc2b1b646d05fbb68565aaadd436801
md-asikuzzaman <mdasikuzzaman.en@example.com>
2024-04-18 16:25:52 +0600

    added update project server type

--- app/api/projects/update/[id]/route.ts
+++ app/api/projects/update/[id]/route.ts
@@ -1,15 +1,22 @@
-import prisma from '@/lib/db';
-import { NextResponse } from 'next/server';
+import prisma from "@/lib/db";
+import { NextResponse } from "next/server";
 
-export async function PUT(req: Request, context: { params: { id: string } }) {
+interface ApiResponse {
+  updatedProject?: ProjectType;
+}
+
+export async function PUT(
+  req: Request,
+  context: { params: { id: string } }
+): Promise<NextResponse<ApiResponse>> {
   const { id } = context.params;
   const { title, description, image, website, github, variant } =
     await req.json();
 
-  const updated = await prisma.project.update({
+  const updatedProject = await prisma.project.update({
     where: { id },
     data: { title, description, image, website, github, variant },
   });
 
-  return NextResponse.json(updated, { status: 200 });
+  return NextResponse.json({ updatedProject }, { status: 200 });
 }
