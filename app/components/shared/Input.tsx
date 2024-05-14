11598f6bc74b2afd60f3951de26ad1848017aac2
md-asikuzzaman <mdasikuzzaman.en@example.com>
2024-05-14 12:33:32 +0600

    import mobile menu into clsx

--- app/components/shared/Input.tsx
+++ app/components/shared/Input.tsx
@@ -1,4 +1,5 @@
-import { NextPage } from 'next';
+import { NextPage } from "next";
+import clsx from "clsx";
 
 interface Props {
   id: string;
@@ -18,41 +19,30 @@ const MyInput: NextPage<Props> = ({
   variant,
 }) => {
   return (
-    <div className='relative'>
+    <div className="relative">
       <input
         type={type}
         id={id}
         onChange={(e) => data(e.target.value)}
         value={value}
-        placeholder=' '
-        className={`
-        mb-4
-        block
-        rounded-md
-        px-6
-        pt-6
-        pb-1
-        w-full
-        text-md
-        appearance-none
-        focus:outline-none
-        focus:ring-0
-        peer ${
+        placeholder=" "
+        className={clsx(
+          "mb-4 block rounded-md px-6 pt-6 pb-1 w-full text-md appearance-none focus:outline-none focus:ring-0 peer",
           variant
-            ? 'text-white bg-neutral-700'
-            : 'text-orange-500 bg-neutral-700'
-        }`}
+            ? "text-white bg-neutral-700"
+            : "text-orange-500 bg-neutral-700"
+        )}
       />
 
       <label
         htmlFor={id}
-        className='
+        className="
         absolute text-md text-zinc-400 duration-150 transform -translate-y-3 scale-75 top-4 left-6 z-10 origin-[0]
         peer-placeholder-shown:scale-100
         peer-placeholder-shown:translate-y-0
         peer-focus:scale-75
         peer-focus:-translate-y-3
-        '
+        "
       >
         {label}
       </label>
--- app/components/shared/MobileMenu.tsx
+++ app/components/shared/MobileMenu.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useMenuActive } from "@/lib/store";
+import clsx from "clsx";
 
 const MobileMenu = () => {
   const { menuActive, setMenuActive } = useMenuActive();
@@ -11,18 +12,20 @@ const MobileMenu = () => {
       className="text-white flex items-center justify-center md:hidden relative h-[40px] w-[40px]  cursor-pointer"
     >
       <span
-        className={`h-[1.5px] w-[28px] bg-white inline-flex absolute transform transition ${
+        className={clsx(
+          "h-[1.5px] w-[28px] bg-white inline-flex absolute transform transition",
           menuActive
             ? "translate-y-[0px] rotate-45"
             : "translate-y-[-6px] rotate-0"
-        }`}
+        )}
       ></span>
       <span
-        className={`h-[1.5px] w-[28px] bg-white inline-flex absolute transform transition ${
+        className={clsx(
+          "h-[1.5px] w-[28px] bg-white inline-flex absolute transform transition",
           menuActive
             ? "translate-y-[0px] -rotate-45"
             : "translate-y-[6px] rotate-0"
-        }`}
+        )}
       ></span>
     </div>
   );
