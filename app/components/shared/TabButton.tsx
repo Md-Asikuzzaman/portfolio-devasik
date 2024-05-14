b22bce7d4c2b55bcc97991b50695a0c11a8300d4
md-asikuzzaman <mdasikuzzaman.en@example.com>
2024-05-14 12:44:17 +0600

    convert tab-button into clsx

--- app/components/shared/TabButton.tsx
+++ app/components/shared/TabButton.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import { NextPage } from "next";
 
 interface Props {
@@ -18,11 +19,12 @@ const TabButton: NextPage<Props> = ({
       onClick={() => {
         setCurrentTab(tabValue);
       }}
-      className={`inline-block p-2 lg:p-3 border-b-2 rounded-t-lg text-md md:text-lg ${
-        currentTab == tabValue
+      className={clsx(
+        "inline-block p-2 lg:p-3 border-b-2 rounded-t-lg text-md md:text-lg",
+        currentTab === tabValue
           ? "text-pink-500 bg-pink-500/10 border-b-2 border-pink-500 rounded-t-lg active"
           : "hover:text-gray-600 hover:border-gray-600 dark:hover:text-gray-300 dark:hover:border-gray-300 border-transparent"
-      } `}
+      )}
     >
       {title}
     </button>
